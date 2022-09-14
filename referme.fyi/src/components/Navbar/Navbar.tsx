7676e68243da7031fb0070bfdf0dab839a992b9e
Matthew Hibbs <89611778+matthibbs7@users.noreply.github.com>
2022-09-14 12:23:50 -0400

    Design spacing

--- referme.fyi/src/components/Navbar/Navbar.tsx
+++ referme.fyi/src/components/Navbar/Navbar.tsx
@@ -7,16 +7,20 @@ import { auth } from '../../firebase/clientApp';
 const Navbar:React.FC = () => {
     const [user, loading, error] = useAuthState(auth)
     return (
-        <Flex bg="#29CC98" height="84px" padding="6px 12px" align="center">
-            <Flex align="center" mr={5}>
-                <Image src="/images/refermefyi2.png" height="64px" />
-                <Link href='/' style={{ textDecoration: 'none' }}>
-                    <Text color="white" fontWeight="800" fontSize="22pt" display={{base: "none", md: 'unset'}}>referme.fyi</Text>
-                </Link>
+        <Flex bg='#29CC98' justifyContent='center'>
+            <Flex bg="#29CC98" height="84px" padding="6px 12px" align="center" width="100%" maxWidth='1250px' minWidth="500px">
+                <Flex align="center" mr={5}>
+                    <Image src="/images/refermefyi2.png" height="64px" minWidth="64px" />
+                    <Link href='/' style={{ textDecoration: 'none' }}>
+                        <Text color="white" fontWeight="800" fontSize="22pt" display={{base: "none", md: 'unset'}}>referme.fyi</Text>
+                    </Link>
+                </Flex>
+                {/* <Directory /> */}
+                <SearchInput />
+                <Flex marginLeft="auto" mr={5}>
+                    <RightContent user={user} />
+                </Flex>
             </Flex>
-            {/* <Directory /> */}
-            <SearchInput />
-            <RightContent user={user} />
         </Flex>
     )
 }
--- referme.fyi/src/components/Navbar/SelectorBar.tsx
+++ referme.fyi/src/components/Navbar/SelectorBar.tsx
@@ -8,11 +8,13 @@ const SelectorBar:React.FC = () => {
     }
 
     return (
-        <Flex height="34px" bg="gray.500">
-            <Button ml={5} height="34px" borderRadius="0px" bg="gray.500" color="white">Referrals By Company</Button>
-            <Button height="34px" borderRadius="0px" bg="gray.500" color="white">Internship Referrals</Button>
-            <Button ml="auto" height="34px" borderRadius="0px" bg="gray.500" color="white">Become a Referee</Button>
-            <Button onClick={toAbout} colorScheme="blackAlpha" height="34px" borderRadius="0px">🙋 About</Button>
+        <Flex justifyContent='center' bg='gray.500'>
+            <Flex height="34px" bg="gray.500" width="100%" maxWidth="1250px">
+                <Button ml={5} height="34px" borderRadius="0px" bg="gray.500" color="white">Referrals By Company</Button>
+                <Button height="34px" borderRadius="0px" bg="gray.500" color="white">Internship Referrals</Button>
+                <Button ml="auto" height="34px" borderRadius="0px" bg="gray.500" color="white">Become a Referee</Button>
+                <Button onClick={toAbout} bg="gray.500" color='white' height="34px" borderRadius="0px" mr={5}>🙋 About</Button>
+            </Flex>
         </Flex>
     )
 }
